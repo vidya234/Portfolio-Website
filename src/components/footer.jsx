364028bb9c9f1b8efcab093cff5482ded91fa934
Vidya <75490044+vidya234@users.noreply.github.com>
2024-04-16 13:16:47 +0530

    changes the name

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
   return (
     <Section crosses className="!px-0 !py-10">
       <div className="container flex sm:justify-between justify-center items-center gap-10 max-sm:flex-col">
-        <p className="text-2xl text-n-4">Vidya</p>
+        <p className="text-2xl text-n-4">Sri Vidya</p>
         <p className="caption text-n-4 lg:block">
           © {new Date().getFullYear()}. All rights reserved.
         </p>
