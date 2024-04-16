2ad9353356df700c88777078f6272f7f945ac264
Vidya <75490044+vidya234@users.noreply.github.com>
2024-04-16 13:11:40 +0530

    made the opennav false

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ import Button from "./Button";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 const Header = () => {
   const pathname = useLocation();
-  const [openNavigation, setOpenNavigation] = useState(true);
+  const [openNavigation, setOpenNavigation] = useState(false);
 
   const toggleNavigation = () => {
     if (openNavigation) {
